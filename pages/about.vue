0e02ebe4ac174828daf3ecb981e835f2d5dba9ee
Yona <dvsscheme@gmail.com>
2023-11-05 12:52:34 +0200

    Add basic layout and assets

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h2>About us</h2>
+        <p>
+            Here at let's meet,
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe magni soluta maxime hic aliquid, amet corporis
+            sit laborum iste voluptates, magnam ducimus blanditiis quod obcaecati asperiores pariatur aspernatur, neque
+            facilis laudantium totam? Soluta architecto doloremque provident dolore, sequi odio. Vero, corporis? Aut
+            eligendi omnis voluptatum atque nemo eum placeat itaque?
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+
+
+        return {}
+    }
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Welcome to let's meet</h2>
+    <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos libero harum suscipit voluptatum fugiat quia
+      sed esse facere doloremque eaque?</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+
+
+    return {}
+  }
+})
+</script>
